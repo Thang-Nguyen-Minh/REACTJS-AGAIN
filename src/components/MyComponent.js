5a2b54e7f08953107c228d4d776db218662bc50d
Minh Thang <ThangNM.B22DCVT525@stu.ptit.edu.vn>
2025-08-10 16:52:06 +0700

    Nesting Component - Vứt hết sang 1 file khác rồi gọi như 1 thẻ HTML ở thằng cha là oke

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,47 +1,12 @@
 import React from 'react'
 import counterReducer from "../redux/reducer/counterReducer";
-
+import UserInfo from "./UserInfo";
 class MyComponent extends React.Component{
-    //this : đại diện cho MyComponent, state là 1 thuộc tính trong đó
-    state = {
-        name : "Minh Thắng",
-        address : "Ha Noi",
-        age: 21
-    }
-    handleClick = (event) => {
-        event.preventDefault();
-        this.setState({
-            name: 'Thanh Bình',
-            age: Math.floor(Math.random()*100)+1//Ngẫu nhiên 1 số từ 1 đến 100
-        })
-    }
-    handleChangeInput = (event) => {
-        event.preventDefault();
-        this.setState({
-            name: event.target.value
-        })
-    }
-    handleSubmit = (event) => {
-        event.preventDefault()
-        //Làm gì có thay đổi trên giao diện mà cho vào setState. Hài vl
-        console.log(this.state);
-    }
+
     render(){
         return (
             <div>
-                My name is {this.state.name}
-                <hr/>
-                My address is {this.state.address}
-                <hr/>
-                My age is {this.state.age}
-                <button onClick={(event) => {this.handleClick(event)}} type="button">Click mee</button>
-                <form onSubmit={(event) => {this.handleSubmit(event)}}>
-                    <input
-                        type="text"
-                        onChange={(event) => {this.handleChangeInput(event)}}
-                    />
-                    <button type="submit">Submit</button>
-                </form>
+                <UserInfo />
             </div>
         )
     }
---
+++ src/components/UserInfo.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react'
+import App from "../App";
+class UserInfo extends Component {
+    //this : đại diện cho MyComponent, state là 1 thuộc tính trong đó
+    state = {
+        name : "Minh Thắng",
+        address : "Ha Noi",
+        age: 21
+    }
+    handleClick = (event) => {
+        event.preventDefault();
+        this.setState({
+            name: 'Thanh Bình',
+            age: Math.floor(Math.random()*100)+1//Ngẫu nhiên 1 số từ 1 đến 100
+        })
+    }
+    handleChangeInput = (event) => {
+        event.preventDefault();
+        this.setState({
+            name: event.target.value
+        })
+    }
+    handleChangeAge = (event) => {
+        event.preventDefault();
+        this.setState({
+            age: event.target.value
+        })
+    }
+    handleSubmit = (event) => {
+        event.preventDefault()
+        //Làm gì có thay đổi trên giao diện mà cho vào setState. Hài vl
+        console.log(this.state);
+    }
+    render() {
+        return (
+            <div>
+                My name is {this.state.name}
+                <hr/>
+                My address is {this.state.address}
+                <hr/>
+                My age is {this.state.age}
+                <button onClick={(event) => {this.handleClick(event)}} type="button">Click mee</button>
+                <form onSubmit={(event) => {this.handleSubmit(event)}}>
+                    <label>Your name : </label>
+                    <input
+                        type="text"
+                        value={this.state.name}
+                        onChange={(event) => {this.handleChangeInput(event)}}
+                    />
+                    <label>Your age : </label>
+                    <input
+                        type="text"
+                        value={this.state.age}
+                        onChange={(event) => {this.handleChangeAge(event)}}
+                    />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+export default UserInfo;
