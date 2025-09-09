b0ee7220b73a1bb04f8ea61d4f376132ea77000b
Minh Thang <ThangNM.B22DCVT525@stu.ptit.edu.vn>
2025-09-09 15:22:23 +0700

    #56 : Modal thêm mới người dùng

---
+++ src/components/Admin/Content/DashBoard.js
@@ -0,0 +1,6 @@
+const DashBoard = (props) => {
+    return (
+        <div>Dashboard</div>
+    )
+}
+export default DashBoard;
---
+++ src/components/Admin/Content/ManageUser.js
@@ -0,0 +1,21 @@
+import ModalCreateUser from "./ModalCreateUser";
+
+const ManageUser = (props) => {
+    return (
+        <div className='manage-user-container'>
+            <div className='title'>
+                Manage User
+            </div>
+            <div className='user-content'>
+                <button>
+                    Add New User
+                </button>
+                <div>
+                    Table User
+                    <ModalCreateUser/>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default ManageUser;
